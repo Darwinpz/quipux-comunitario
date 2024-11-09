45d0c3f2e1a74384c5967204f1b4490f7e304ada
Darwinpz <dpilaloazea@gmail.com>
2024-11-09 16:56:06 -0500

    corrección server html_a_pdf
    descartando variables de entorno en servidor html_a_pdf

--- config.php
+++ config.php
@@ -21,6 +21,7 @@
 
 // Las contraseñas y datos importantes se ocultan en un archivo ".env".
 require_once 'cargar_env.php';
+
 // Archivo con algunas configuraciones de términos usados en el sistema
 $FILE_LOCAL = getenv('FILE_LOCAL');
 
@@ -90,7 +91,6 @@ $nombre_servidor_respaldos = getenv('nombre_servidor_respaldos'); // en caso que
 $servidor_firma = getenv('servidor_firma');
 $servidor_viajes = getenv('servidor_viajes');
 $servidor_pdf = getenv('servidor_pdf');
-$tipo_sistema = getenv('tipo_sistema');
 
 //Numero Meses en Reportes
 $numeroMeses = getenv('numeroMeses');
--- html_a_pdf/config.php
+++ html_a_pdf/config.php
@@ -1,4 +1,4 @@
 <?php
-	$nombre_servidor = getenv('servidor_pdf');
-    $tipo_sistema = getenv('tipo_sistema');
+	$nombre_servidor = "http://127.0.0.1/quipux-comunitario/html_a_pdf";
+    $tipo_sistema = "Produccion";
 ?>
