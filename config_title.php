8cde75ae136862deca8008c0a8fd321d4880f9ab
Darwinpz <dpilaloazea@gmail.com>
2026-01-19 19:51:15 -0500

    Update config_title.php

--- config_title.php
+++ config_title.php
@@ -2,7 +2,7 @@
 $institucionSigla="BMV";
 $institucionNombre="Instituto Superior Tecnológico Bolivar Madero Vargas";
 $banner1 = "imagenes/index/banner_fijo_index.png";
-$banner2 = "imagenes/index/banner_fijo_index2.png";
+$banner2 = "imagenes/index/banner_fijo_index2.jpg";
 $linkBanner1 = "";
 $nombreLinkBanner1 = "";
 $linkBanner2 = "";
